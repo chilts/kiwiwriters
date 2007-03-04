3f363d06f0e1f7f3863e02a2eec38101114a8ef3
andy <andy@a1b55a4a-4c24-0410-8ffd-c32dbf0953b7>
2007-03-04 05:37:26 +0000

    - database patch 9->10 for the Forum Signature

---
+++ db/patch_9_10.sql
@@ -0,0 +1,82 @@
+BEGIN;
+
+-- ----------------------------------------------------------------------------
+
+-- update the forum.tables to have the account information
+CREATE TABLE forum.info (
+    account_id      INTEGER NOT NULL REFERENCES account.account PRIMARY KEY,
+    posts           INTEGER NOT NULL DEFAULT 0,
+    signature       TEXT NOT NULL DEFAULT '',
+
+    LIKE base       INCLUDING DEFAULTS
+);
+
+-- function: newtopic
+CREATE OR REPLACE FUNCTION newtopic() RETURNS trigger as '
+    BEGIN
+        UPDATE forum.forum SET topics = topics + 1 WHERE id = NEW.forum_id;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+DROP TRIGGER newtopic_inserted ON forum.topic;
+CREATE TRIGGER newtopic_inserted BEFORE INSERT ON forum.topic
+    FOR EACH ROW EXECUTE PROCEDURE newtopic();
+
+-- function: deltopic
+CREATE OR REPLACE FUNCTION deltopic() RETURNS trigger as '
+    BEGIN
+        UPDATE forum.forum SET topics = topics - 1 WHERE id = OLD.forum_id;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+DROP TRIGGER deltopic_deleted ON forum.topic;
+CREATE TRIGGER deltopic_deleted AFTER DELETE ON forum.topic
+    FOR EACH ROW EXECUTE PROCEDURE deltopic();
+
+-- function: newpost
+CREATE OR REPLACE FUNCTION newpost() RETURNS trigger as '
+    DECLARE
+        -- to hold the account id of the person posting
+        found_id INTEGER;
+    BEGIN
+        UPDATE forum.topic SET posts = posts + 1, poster_id = NEW.account_id WHERE id = NEW.topic_id;
+        UPDATE forum.forum SET posts = posts + 1, poster_id = NEW.account_id WHERE id = (SELECT forum_id FROM forum.topic WHERE id = NEW.topic_id);
+
+        -- now update this users number of posts
+        SELECT INTO found_id account_id FROM forum.info WHERE account_id = NEW.account_id;
+        IF NOT FOUND THEN
+            INSERT INTO forum.info(account_id) VALUES(NEW.account_id);
+            SELECT INTO found_id account_id FROM forum.info WHERE account_id = NEW.account_id;
+        END IF;
+        UPDATE forum.info SET posts = posts + 1 WHERE account_id = found_id;
+
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+DROP TRIGGER newpost_inserted ON forum.post;
+CREATE TRIGGER newpost_inserted BEFORE INSERT ON forum.post
+    FOR EACH ROW EXECUTE PROCEDURE newpost();
+
+-- function: delpost
+CREATE OR REPLACE FUNCTION delpost() RETURNS trigger as '
+    BEGIN
+        UPDATE forum.topic SET posts = posts - 1 WHERE id = OLD.topic_id;
+        UPDATE forum.forum SET posts = posts - 1 WHERE id = (SELECT forum_id FROM forum.topic WHERE id = OLD.topic_id);
+        UPDATE forum.info SET posts = posts - 1 WHERE account_id = OLD.account_id;
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+DROP TRIGGER delpost_deleted ON forum.post;
+CREATE TRIGGER delpost_deleted AFTER DELETE ON forum.post
+    FOR EACH ROW EXECUTE PROCEDURE delpost();
+
+-- copy across the current number of posts
+INSERT INTO forum.info(account_id, posts) SELECT account_id, total FROM forum.count;
+
+DROP TABLE forum.count CASCADE;
+
+-- ----------------------------------------------------------------------------
+
+UPDATE property SET value = 10 WHERE key = 'Patch Level';
+
+COMMIT;
