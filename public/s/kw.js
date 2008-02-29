0104e808bc32a8370d497498a0d1ae4e74c434f0
andy <andy@a1b55a4a-4c24-0410-8ffd-c32dbf0953b7>
2008-02-29 10:51:19 +0000

    - add jQuery for use in KiwiWriters :-)

--- public/s/kw.js
+++ public/s/kw.js
@@ -1,5 +1,6 @@
 /* toggles 'hide' on 'name', 'name-b1' and 'name-b2' (for button 1 and 2) */
 function toggle3(name) {
-    toggleElementClass('hide', name, name + '-b1', name + '-b2');
+    var ids = "#" + name + ", #" + name + "-b1, #" + name + "-b2";
+    $(ids).toggleClass('hide');
     return false;
 }
